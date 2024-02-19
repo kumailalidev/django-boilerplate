3f48abbec17f4c1b66afbcc17ae9e5aa22915d6a
kumailalidev <kumailalidev@gmail.com>
2024-02-20 04:30:52 +0500

    Added /favicon.ico url

--- src/project/urls.py
+++ src/project/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 
 DEBUG = settings.DEBUG
 
 urlpatterns = [
+    path("favicon.ico", RedirectView.as_view(url="/static/images/icons/favicon.ico")),
     path("admin/", admin.site.urls),
     path("greetings/", include("greetings.urls")),
 ]
