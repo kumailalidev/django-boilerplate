c5a920b32079eef0dbc80d060cc7435b64503bf7
kumailalidev <kumailalidev@gmail.com>
2024-02-21 21:10:16 +0500

    Updated greetings app.

--- src/greetings/models.py
+++ src/greetings/models.py
@@ -1,1 +1,8 @@
 from django.db import models
+
+
+class Greeting(models.Model):
+    message = models.TextField(verbose_name="Greeting message", max_length=255)
+
+    def __str__(self):
+        return str(self.message[0:50] + "...")
--- src/greetings/views.py
+++ src/greetings/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
 
+from greetings.models import Greeting
+
 
 def home(request):
-    return render(request=request, template_name="greetings/index.html", context={})
+    message = "No Message..."
+
+    # get latest greeting obj. from db
+    greeting_obj = Greeting.objects.last()
+
+    if greeting_obj:
+        message = greeting_obj.message
+
+    context = {
+        "message": message,
+    }
+
+    return render(
+        request=request, template_name="greetings/index.html", context=context
+    )
