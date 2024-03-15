1cde3c9e1ccf06ab9df558e49d9e51707f1fc5ff
kumailalidev <kumailalidev@gmail.com>
2024-03-16 04:20:02 +0500

    Updated development settings.

--- config/settings/development.py
+++ config/settings/development.py
@@ -1,29 +1,58 @@
+"""
+Django development settings
+"""
+
 from .base import *
+from .base import env
 
 # GENERAL
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#debug
 DEBUG = True
+# https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env.str(
     "DJANGO_SECRET_KEY",
     default="django-insecure-^uz2g$=t%t)i=3aas2@_ia94q*(&d2m4z7xczlq&@*fn)y*hv6",
 )
+# https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
 
-# APPS
-INSTALLED_APPS += [
-    "django_extensions",
-    "debug_toolbar",
-]
-
-# MIDDLEWARES
-MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]
-
-# Django Debug Toolbar
-INTERNAL_IPS = [
-    "127.0.0.1",
-]
+# CACHES
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#caches
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",  # Default
+        "LOCATION": "",  # Default
+    }
+}
 
 # EMAIL
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
 EMAIL_BACKEND = env(
     "DJANGO_EMAIL_BACKEND",
     default="django.core.mail.backends.console.EmailBackend",
 )
+
+# INSTALLED APPS
+# ------------------------------------------------------------------------------
+INSTALLED_APPS += [
+    # https://django-extensions.readthedocs.io/en/latest/
+    "django_extensions",
+    # https://django-debug-toolbar.readthedocs.io/en/latest/
+    "debug_toolbar",
+]
+# django-debug-toolbar
+# ------------------------------------------------------------------------------
+# https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#configure-internal-ips
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
+# MIDDLEWARES
+# ------------------------------------------------------------------------------
+MIDDLEWARE += [
+    # https://django-debug-toolbar.readthedocs.io/en/latest/
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+]
