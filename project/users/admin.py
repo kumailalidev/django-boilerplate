93c4cbe399364d9f2dc8f3813804e1e08f5a0e5c
kumailalidev <kumailalidev@gmail.com>
2024-03-18 02:54:26 +0500

    Registered custom user model in admin and updated app name.

--- project/users/admin.py
+++ project/users/admin.py
@@ -1,3 +1,80 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from .forms import UserCreationForm, UserChangeForm
+
+UserModel = get_user_model()
+
+
+@admin.register(UserModel)
+class UserAdmin(BaseUserAdmin):
+    """
+    Admin class for custom user model
+    """
+
+    fieldsets = (
+        (
+            "User Information",
+            {
+                "fields": (
+                    "username",
+                    "email",
+                    "password",
+                )
+            },
+        ),
+        (
+            _("Personal Information"),
+            {"fields": ("first_name", "last_name")},
+        ),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            "Required Information",
+            {
+                "fields": (
+                    "username",
+                    "email",
+                    "password1",
+                    "password2",
+                ),
+            },
+        ),
+        (
+            "Optional Information",
+            {
+                "fields": ("first_name", "last_name"),
+            },
+        ),
+    )
+    form = UserChangeForm
+    add_form = UserCreationForm
+    list_display = (
+        "username",
+        "email",
+        "date_of_birth",
+        "is_staff",
+        "is_verified",
+    )
+    list_filter = ("is_staff", "is_superuser", "is_active", "groups")
+    search_fields = ("username", "email", "first_name", "last_name")
+    ordering = ("username", "email")
+    filter_horizontal = (
+        "groups",
+        "user_permissions",
+    )
