03f753fa9d23b2d0f370c746557375c18f5d6600
kumailalidev <kumailalidev@gmail.com>
2024-03-18 18:44:46 +0500

    Created login view, url and template.

--- project/users/urls.py
+++ project/users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import user_signup_view
+from .views import user_signup_view, user_login_view
 
 app_name = "users"
 
 urlpatterns = [
     path(route="signup/", view=user_signup_view, name="signup"),
+    path(route="login/", view=user_login_view, name="signup"),
 ]
--- project/users/views.py
+++ project/users/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponseRedirect
 from django.conf import settings
 from django.contrib.sites.shortcuts import get_current_site
+from django.contrib.auth import login as auth_login
 from django.shortcuts import resolve_url
 from django.utils.decorators import method_decorator
 from django.utils.translation import gettext_lazy as _
@@ -11,7 +12,7 @@ from django.views.generic.edit import FormView
 from django.contrib import messages
 
 from .mixins import RedirectURLMixin
-from .forms import UserCreationForm
+from .forms import UserCreationForm, AuthenticationForm
 
 LOGIN_REDIRECT_URL = settings.LOGIN_REDIRECT_URL
 
@@ -79,3 +80,65 @@ class UserSignUpView(RedirectURLMixin, FormView):
 
 
 user_signup_view = UserSignUpView.as_view()
+
+
+class UserLoginView(RedirectURLMixin, FormView):
+    """
+    Display the login form and handle the login action.
+    """
+
+    form_class = AuthenticationForm
+    authentication_form = None
+    template_name = "registration/login.html"
+    redirect_authenticated_user = True
+    extra_context = None
+
+    @method_decorator(sensitive_post_parameters())
+    @method_decorator(csrf_protect)
+    @method_decorator(never_cache)
+    def dispatch(self, request, *args, **kwargs):
+        if self.redirect_authenticated_user and self.request.user.is_authenticated:
+            redirect_to = self.get_success_url()
+            if redirect_to == self.request.path:
+                raise ValueError(
+                    "Redirection loop for authenticated user detected. Check that "
+                    "your LOGIN_REDIRECT_URL doesn't point to a signup page."
+                )
+            return HttpResponseRedirect(redirect_to)
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_default_redirect_url(self):
+        """Return the default redirect URL."""
+        if self.next_page:
+            return resolve_url(self.next_page)
+        else:
+            return resolve_url(LOGIN_REDIRECT_URL)
+
+    def get_form_class(self):
+        return self.authentication_form or self.form_class
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs["request"] = self.request
+        return kwargs
+
+    def form_valid(self, form):
+        """Security check complete. Log the user in."""
+        auth_login(self.request, form.get_user())
+        return HttpResponseRedirect(self.get_success_url())
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        current_site = get_current_site(self.request)
+        context.update(
+            {
+                self.redirect_field_name: self.get_redirect_url(),
+                "site": current_site,
+                "site_name": current_site.name,
+                **(self.extra_context or {}),
+            }
+        )
+        return context
+
+
+user_login_view = UserLoginView.as_view()
