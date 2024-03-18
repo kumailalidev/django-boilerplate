1cbd79dd0906141c2e4ba1069129820a4d8179d1
kumailalidev <kumailalidev@gmail.com>
2024-03-18 19:28:37 +0500

    Bug fixes and removed unused templates.

--- project/users/urls.py
+++ project/users/urls.py
@@ -6,5 +6,5 @@ app_name = "users"
 
 urlpatterns = [
     path(route="signup/", view=user_signup_view, name="signup"),
-    path(route="login/", view=user_login_view, name="signup"),
+    path(route="login/", view=user_login_view, name="login"),
 ]
