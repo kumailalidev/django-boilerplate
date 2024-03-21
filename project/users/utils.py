fcdadee7ce83e777571d33d50b38bcc7ddfa7475
kumailalidev <kumailalidev@gmail.com>
2024-03-22 04:03:05 +0500

    Created utility function send_mail for sending email address to multiple users.

--- project/users/utils.py
+++ project/users/utils.py
@@ -1,11 +1,16 @@
 from django.http import HttpResponseRedirect, QueryDict
 from django.shortcuts import resolve_url
 from django.conf import settings
-from django.contrib.auth import REDIRECT_FIELD_NAME
+from django.template import loader
+from django.contrib.auth import REDIRECT_FIELD_NAME, get_user_model
+from django.template import loader
+from django.core.mail import EmailMultiAlternatives
 
 from urllib.parse import urlparse
 
 from .views import UserLogoutView
+
+UserModel = get_user_model()
 
 
 def logout_the_login(request, login_url=None):
@@ -29,3 +34,28 @@ def redirect_to_login(next, login_url=None, redirect_field_name=REDIRECT_FIELD_N
         login_url_parts[4] = querystring.urlencode(safe="/")
 
     return HttpResponseRedirect(urlparse(login_url_parts))
+
+
+def send_mail(
+    subject_template_name,
+    email_template_name,
+    context,
+    from_email,
+    to_email,
+    html_email_template_name=None,
+):
+    """
+    Email message which can be sent to multiple users.
+    Send a django.core.mail.EmailMultiAlternatives to `to_email`.
+    """
+    subject = loader.render_to_string(subject_template_name, context)
+    # Email subject *must not* contain newlines
+    subject = "".join(subject.splitlines())
+    body = loader.render_to_string(email_template_name, context)
+
+    email_message = EmailMultiAlternatives(subject, body, from_email, [to_email])
+    if html_email_template_name is not None:
+        html_email = loader.render_to_string(html_email_template_name, context)
+        email_message.attach_alternative(html_email, "text/html")
+
+    email_message.send()
