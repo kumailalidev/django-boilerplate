2d1ab852b9c2574715f32a91bf800c89bb9fcfd9
kumailalidev <kumailalidev@gmail.com>
2024-03-23 05:50:23 +0500

    Added 400 Bad Request page.

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views import defaults as default_views
 from django.views.generic.base import RedirectView
 
 DEBUG = settings.DEBUG
@@ -26,3 +27,12 @@ if DEBUG:
         urlpatterns += [
             path("__debug__/", include(debug_toolbar.urls)),
         ]
+    # This allows the error pages to be debugged during development, just visit
+    # these url in browser to see how these error pages look like.
+    urlpatterns += [
+        path(
+            "400/",
+            default_views.bad_request,
+            kwargs={"exception": Exception("Permission Denied")},
+        )
+    ]
